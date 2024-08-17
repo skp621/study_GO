21d81e3059a9c964e40dc8bdcf30e6510e0af462
skp621 <s.e.work.cat@gmail.com>
2024-08-17 10:50:41 +0900

    update:main.go コーディング

--- practice_02/main.go
+++ practice_02/main.go
@@ -1,9 +1,35 @@
+//**************************************************
+//
+//  FizzBuzz
+//										2024.08.17
+//**************************************************
+
 package main
 
 import "fmt"
 
 func main() {
 
-	fmt.Println("仕事の時間だ、621")
+	for i := 1; i < 21; i++ { // perform varying i from 1 by 1 until i > 20
+
+		if i%3 == 0 && i%5 == 0 { // and ではない &&
+
+			fmt.Println("FizzBuzz")
+
+		} else if i%3 == 0 {
+
+			fmt.Println("Fizz")
+
+		} else if i%5 == 0 {
+
+			fmt.Println("Buzz")
+
+		} else {
+
+			fmt.Println(i)
+
+		}
+
+	}
 
 }
