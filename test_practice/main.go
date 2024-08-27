63119098e87169362322a585ad9112cfe49e1047
Yoshi <s.e.work.cat@gmail.com>
2024-08-27 12:28:08 +0900

    Add: ローカル検証用のファイル作成

---
+++ test_practice/main.go
@@ -0,0 +1,17 @@
+package main
+
+// 足し算関数
+
+func Add(a, b int) int {
+
+	return a + b
+
+}
+
+// 掛け算関数
+
+func Mul(a, b int) int {
+
+	return a * b
+
+}
--- test_practice/main_test.go
+++ test_practice/main_test.go
@@ -1,10 +1,3 @@
-// **************************************************
-//
-//	足し算 関数 TEST 用
-//
-//	   2024.08.25
-//
-// **************************************************
 package main
 
 import "testing"
@@ -29,18 +22,24 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-func TestErrorAdd(t *testing.T) {
+// TestMul 関数のテスト
+func TestMul(t *testing.T) {
 	tests := []struct {
 		a, b     int
 		expected int
 	}{
-		{1, 1, 3},
+		{2, 3, 6},    // 2 * 3 = 6
+		{-2, 3, -6},  // -2 * 3 = -6
+		{0, 5, 0},    // 0 * 5 = 0
+		{7, 0, 0},    // 7 * 0 = 0
+		{-4, -5, 20}, // -4 * -5 = 20
+		{1, 1, 1},    // 1 * 1 = 1
 	}
 
 	for _, test := range tests {
-		result := Add(test.a, test.b)
+		result := Mul(test.a, test.b)
 		if result != test.expected {
-			t.Errorf("Add(%d, %d) != %d; expected %d", test.a, test.b, result, test.expected)
+			t.Errorf("Mul(%d, %d) = %d; expected %d", test.a, test.b, result, test.expected)
 		}
 	}
 }
