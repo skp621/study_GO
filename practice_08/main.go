7eaa597251cc5da295b596d5622eb583fd5536bb
skp621 <s.e.work.cat@gmail.com>
2024-09-09 20:58:15 +0900

    update:ターミナルで確認

--- practice_08/main.go
+++ practice_08/main.go
@@ -18,33 +18,33 @@ func main() {
 	var a, b int
 	var c string
 
-	// 最初の数値を入力
+	fmt.Println("左辺を入力してください")
 	fmt.Scanln(&a)
 
-	// 演算子を入力
+	fmt.Println("演算子を入力してください")
 	fmt.Scanln(&c)
 
-	// 次の数値を入力
+	fmt.Println("右辺を入力してください")
 	fmt.Scanln(&b)
 
 	// 演算子に応じて計算を実行
 	switch c {
 	case "+":
 		result := kansu.Add(a, b)
-		fmt.Println("結果:", result)
+		fmt.Println("答え:", result)
 	case "-":
 		result := kansu.Sub(a, b)
-		fmt.Println("結果:", result)
+		fmt.Println("答え:", result)
 	case "*":
 		result := kansu.Mul(a, b)
-		fmt.Println("結果:", result)
+		fmt.Println("答え:", result)
 	case "/":
 		// 割り算でエラー処理
 		result, err := kansu.Div(a, b)
 		if err != nil {
 			fmt.Println("エラー:", err)
 		} else {
-			fmt.Println("結果:", result)
+			fmt.Println("答え:", result)
 		}
 	default:
 		fmt.Println("無効な演算子が入力されました。+、-、*、/ のいずれかを入力してください。")
